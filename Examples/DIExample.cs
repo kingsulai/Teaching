2a4afa56c3e869e2a2fd00e92e069b3e8f474538
sulai <sulai@DESKTOP-I2L75NN>
2021-02-04 19:12:25 +0100

    DI und custom Logger

---
+++ Examples/DIExample.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching.Examples
+{
+    public interface IDIExample
+    {
+        void LogSomething();
+    }
+
+    public class DIExample : IDIExample
+    {
+        private ILogger logger;
+
+        public DIExample(ILoggerFactory f)
+        {
+            logger = f.CreateLogger("Debug");
+        }
+
+        public void LogSomething()
+        {
+            logger.LogInformation("Test");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Lamar;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -11,15 +12,23 @@ namespace Teaching
         static void Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
+                .AddLogging(c => c.AddProvider(new MyLoggerProvider())) //c.AddConsole())
+                .AddSingleton(typeof(IDIExample),typeof( DIExample))
                 .BuildServiceProvider();
-
             /* Commenting Trick
             var e = new Example1();
             /*/
             IExample e = new Example1();
             //*/
 
+            var diExample = serviceProvider.GetService<IDIExample>();
+            diExample.LogSomething();
             e.Run();
+
+            StreamWriter w = new StreamWriter("log.txt");
+            w.WriteLine("TEST");
+            w.Flush();
+            w.Close();
 
         }
     }
@@ -47,5 +56,47 @@ namespace Teaching
 
 
         public abstract void Run();
+    }
+
+    public class FileLogger : ILogger
+    {
+        public StreamWriter fstream;
+
+        public FileLogger()
+        {
+            fstream = new StreamWriter("test.log");
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return fstream;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            fstream.WriteLine(formatter(state,exception)+"TEST");
+            fstream.Flush();
+        }
+    }
+    public class MyLoggerProvider : ILoggerProvider
+    {
+        private FileLogger fileLogger;
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            fileLogger = new FileLogger();
+            return fileLogger;
+        }
+
+        public void Dispose()
+        {
+            fileLogger.fstream.Flush();
+            fileLogger.fstream.Close();
+        }
     }
 }
