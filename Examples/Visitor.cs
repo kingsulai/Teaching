a3a2f243ba31837d78281f84de156f1f9c306265
sulai <sulai@DESKTOP-I2L75NN>
2021-02-04 16:51:12 +0100

    Visitor Pattern

---
+++ Examples/Visitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teaching.Examples
+{
+    public interface Visitor
+    {
+        void VistProduct(Product p);
+    }
+
+    public class VisitorBox : Box
+    {
+        public void VisitProducts(Visitor v)
+        {
+            foreach(var p in products)
+            {
+                switch (p)
+                {
+                    case Product p1:
+                        v.VistProduct(p1);
+                        break;
+                    case VisitorBox vb:
+                        vb.VisitProducts(v);
+                        break;
+                }
+            }
+        }
+    }
+}
