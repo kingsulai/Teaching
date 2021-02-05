5c7461507380a109e7739e5ffbd5ce08d6610503
sulai <sulai@DESKTOP-I2L75NN>
2021-02-05 16:32:27 +0100

    Visitor not implementedViisiitor not implemented

--- Teaching/Examples/IVisitor.cs
+++ Teaching/Examples/IVisitor.cs
@@ -4,21 +4,17 @@ using System.Text;
 
 namespace Teaching.Examples
 {
-    public interface Visitor
-    {
-        void VistProduct(Product p);
-    }
 
     public class VisitorBox : Box
     {
-        public void VisitProducts(Visitor v)
+        public void VisitProducts(Action<Product> v)
         {
             foreach(var p in products)
             {
                 switch (p)
                 {
                     case Product p1:
-                        v.VistProduct(p1);
+                        v(p1);
                         break;
                     case VisitorBox vb:
                         vb.VisitProducts(v);
